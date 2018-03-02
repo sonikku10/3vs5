7822862175c9be750d3415a14080a898d869f7cd
sonikku10 <lee.melvin@knights.ucf.edu>
2018-03-01 22:34:41 -0500

    Initial code w/ for loop

--- untitled/Project Euler #1.py
+++ untitled/Project Euler #1.py
@@ -7,14 +7,15 @@ print("")
 # Input
 x = int(input("Go ahead and enter a positive integer: "))
 
+# Set your current sum
 my_sum = 0
 
 for i in range(0, x):
     if i % 3 == 0 or i % 5 == 0:
         my_sum += i
         i += 1
+# The print() function goes out side of your loop so it doesn't display each iteration up to the end.
+# It will only display the final iteration.
 print(my_sum)
-
-# This is out of your while loop so that all the sums aren't displayed.
 
 
